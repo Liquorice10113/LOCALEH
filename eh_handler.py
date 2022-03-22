6a8c58886bc16e28ddaee22b8a39399a1623ba32
Liquorice10113 <licoqi2@gmail.com>
2022-03-23 03:07:49 +0800

    Commit.

--- eh_handler.py
+++ eh_handler.py
@@ -1,30 +1,181 @@
+import time
+import requests as req
+from bs4 import BeautifulSoup
+import re,os
+from html import unescape
+import json
+
+BASE = './COMIC/'
+
+C = { "headers": {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36"}, "proxies": {"http": "http://127.0.0.1:10809", "https": "http://127.0.0.1:10809"}, "cookies": {"__cfduid": "d3a4b57bde61a4b17c12d649f9bb44e521620030396", "event": "1620131041", "hath_perks": "m1.m2.m3.a.t1.t2.p1.s.q-b74fce7784", "ipb_member_id": "4051477", "ipb_pass_hash": "928b26b5503552470761db30ccd71fb7", "sk": "g432vw9uvmwdrxpserwmyokq6na2", "sl": "dm_1","nw":"1"}}
+
+
 class MetaUtil():
     def __init__(self) -> None:
         return
     def newMeta(self):
         meta = dict()
+        meta['title'] = None
+        meta['url'] = None
+        meta['length'] = None
+        meta['modified'] = time.time()
+        meta['status'] = PENDING
+        meta['tags'] = []
         return meta
 
 M = MetaUtil()
+
+status = [ 'PENDING','DOWNLOADING','DONE','FINISHED','ONGOING' ]
+
 PENDING = 0
 DOWNLOADING = 1
 DONE = 2
 
+FINISHED = 3
+ONGOING = 4
+
+#C = dict()
+
+def wash(s):
+    s = unescape(s)
+    for i in "\/:*?\"<>|":
+        s = s.replace(i, '')
+    return s
+
+
+def uniCnt(cnt):
+    return (3-len(str(cnt)))*'0'+str(cnt)
+
+def sw(s,*args):
+    return s
+
+def log(s):
+    print(time.ctime(),s)
+
+def get(url, binary=False, stream=False):
+    # print(url)
+    global C
+    if "509.gif" in url:
+        log("Limits reached maybe?")
+    rc = 1
+    while True:
+        try:
+            resp = req.get(url, headers=C['headers'], cookies=C['cookies'], proxies=C['proxies'])
+            if binary:
+                return resp.content
+            else:
+                return resp.text
+        except KeyboardInterrupt:
+            log('KeyboardInterrupt')
+            raise
+        except Exception as e:
+            raise
+            print(e)
+            if rc > 6:
+                #raise Exception("Download Failed.")
+                log('Failed after 6 attemps.')
+                raise
+                return b''
+            time.sleep(5)
+            log('Retrying')
+            rc += 1
+
+
 class Gallery():
-    def __init__(self,url) -> None:
+    def __init__(self,url=None) -> None:
         self.url = url
         self.meta = M.newMeta()
+        self.meta['url'] = url
+        self.images = []
+        self.html = ''
     def checkLocal(self):
         pass
     def parse(self):
-        pass
+        try:
+            self.html = get(self.url)
+            soup = BeautifulSoup(self.html, 'html.parser')
+            
+            self.meta['title'] = wash(soup.title.text.replace(
+                ' - E-Hentai Galleries', ''))
+            print(self.meta['title'])
+            self.meta['length'] = int(re.search('(\d+) pages', self.html).group(1))
+
+            pageUrls = soup.find(class_="ptt").find_all('a')
+            pageUrls = [u.attrs['href'] for u in pageUrls]
+            pageUrls = sorted(list(set(pageUrls)))
+            for galleryPageUrl in pageUrls:
+                self.html = get(galleryPageUrl)
+                soup = BeautifulSoup(self.html, 'html.parser')
+                for div in soup.find_all('div', class_='gdtm'):
+                    index_ = uniCnt(int(div.div.a.img.attrs['alt'])-1)
+                    name = re.match('Page \d+: (.+)',
+                                    div.div.a.img.attrs['title']).group(1)
+                    pageUrl = div.div.a.attrs['href']
+                    page = Img(pageUrl,name,self.meta)
+                    self.images.append(page)
+            if not os.path.exists( os.path.join( BASE,self.meta['title'] ) ):
+                os.makedirs(os.path.join( BASE,self.meta['title'] ) )
+            self.save2disk()
+        except Exception as e:
+            raise
+            print(e)
+            print(sw("Parse failed for:\n"+self.url, c="red"))
+            self.errored = True
     def downloadPipe(self):
         pass
     def report(self):
-        pass
+        data = dict()
+        data['modified'] = self.meta['modified']
+        data['title'] = self.meta['title']
+        data['thumb'] = self.images[0].name
+        data['length'] = self.meta['length']
+        data['status'] = self.meta['status']
+        return data
+    def detail(self):
+        data = dict()
+        data['modified'] = self.meta['modified']
+        data['title'] = self.meta['title']
+        data['thumb'] = self.images[0].name
+        data['status'] = self.meta['status']
+        data['length'] = self.meta['length']
+        data['tags'] = self.meta['tags']
+        return data
+    def images_detail(self):
+        data = []
+        for img in self.images:
+            data.append({
+                'name':img.name,
+                'url':img.url,
+                'status':img.status
+            })
+    def save2disk(self):
+        data = dict()
+        data['meta'] = self.meta
+        data['images'] =[p.dump() for p in  self.images]
+        meta_fn = os.path.join(BASE,self.meta['title'],'gallery.json')
+        json.dump(data,open(meta_fn,'w'))
+    def loadFromLocal(self,meta_fn):
+        data = json.load(open(meta_fn,'r'))
+        self.meta = data['meta']
+        self.url = self.meta['url']
+        for img in data['images']:
+            fn = img['file_name']
+            url = img['url']
+            img = Img(url,fn,self.meta)
+            if os.path.exists( os.path.join(BASE,self.meta['title'],fn) ):
+                img.status = DONE
+            self.images.append(img)
 
 class Img():
-    def __init__(self) -> None:
-        pass
+    def __init__(self,url,name,parentMeta,status=PENDING) -> None:
+        self.url = url
+        self.name = name
+        self.parentMeta = parentMeta
+        self.status = status
     def download(self):
         pass
+    def dump(self):
+        data = dict()
+        data['file_name'] = self.name
+        data['url'] = self.url
+        return data
--- service.py
+++ service.py
@@ -1,10 +1,80 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify, send_from_directory
+from threading import Thread
+from eh_handler import *
 
-Galleries = []
+class Worker(Thread):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+    def run(self):
+        global galleries_url_in, galleries
+        while True:
+            if len(galleries_url_in) >0:
+                url_in = galleries_url_in.pop(0)
+                gid = re.search('e-hentai.org\/g\/(\w+\/\w+\/)',url_in).group(1)
+                #gid = gid.replace('/','')
+                if gid in galleries:
+                    galleries[gid].meta['status'] = PENDING
+                    downQ.append(gid)
+                else:
+                    g = Gallery(url_in)
+                    g.meta['title'] = 'Parsing...'
+                    galleries[gid] = g
+                g.parse()
+            time.sleep(1)
+
+def loadFromLocal():
+    for folder in os.listdir(BASE):
+        if not os.path.isdir( os.path.join(BASE,folder) ):
+            continue
+        if not os.path.isfile( os.path.join(BASE,folder,'gallery.json') ):
+            continue
+        g = Gallery()
+        g.loadFromLocal(os.path.join(BASE,folder,'gallery.json'))
+        gid = re.search('e-hentai.org\/g\/(\w+\/\w+\/)',g.meta['url']).group(1)
+        galleries[gid] = g
+
+#galleries_url_in = ['https://e-hentai.org/g/2172783/f13044f2c5/']
+galleries_url_in = []
+downQ = []
+galleries = dict()
+
+loadFromLocal()
+
+w = Worker()
+w.start()
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def gallery_index():
+    global galleries
+    resp = ''
+    seg = '<a href="/g/{}">{}</a></br>'
+    for gid in galleries:
+        g = galleries[gid]
+        resp += seg.format(gid,g.meta['title'] )
+    return resp
+
+@app.route("/g/<path:gid>")
+def gallery(gid):
+    #gid = gid.replace('/','')
+    if not gid[-1] == '/':
+        gid = gid +'/'
+    seg = '<a href="/s/{0}/{1}">{1}</a>{2}</br>'
+    if gid in galleries:
+        resp = galleries[gid].meta['title']+'</br>'
+        for img in galleries[gid].images:
+            resp += seg.format(galleries[gid].meta['title'],img.name,status[img.status])
+        return resp
+    return 404
+
+@app.route("/s/<title>/<fn>")
+def image(title,fn):
+    if os.path.exists(os.path.join(BASE,title,fn)):
+        return send_from_directory( os.path.join(BASE,title),fn )
+    else:
+        return 404
+
+app.run('0.0.0.0',8080,debug=True)
+
